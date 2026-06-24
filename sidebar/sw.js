b216518ff3a120f1e1fa7658127f67799d77d732
J7892 <70664615+J7892@users.noreply.github.com>
2026-06-24 13:31:17 -0400

    Add files via upload

---
+++ sidebar/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'notes-tasks-v4';
+const ASSETS_TO_CACHE = [
+  '/sidebar.html',
+  '/quill.snow.css',
+  '/quill.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
